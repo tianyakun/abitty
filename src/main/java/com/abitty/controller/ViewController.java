01192e89760c6d72492c0953939e7368650bf794
yakun.tian <yakun.tian@aliyun.com>
2017-06-29 11:19:14 +0800

    edit

--- src/main/java/com/abitty/controller/ViewController.java
+++ src/main/java/com/abitty/controller/ViewController.java
@@ -1,10 +1,13 @@
 package com.abitty.controller;
 
 import com.abitty.entity.TblUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -13,14 +16,23 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class ViewController {
 
+    private final static Logger logger = LoggerFactory.getLogger(ViewController.class);
+
     @RequestMapping("/loginIndex")
-    public String loginIndex(Model model) {
+    public String loginIndex(Model model, final HttpServletRequest request) {
+
+        String requestUri = request.getRequestURI();
+        logger.info("登录请求 requestUri={}", requestUri);
+
         model.addAttribute("uid", "");
         return "loginIndex";
     }
 
     @RequestMapping("/view/**")
-    public String view(Model model, HttpSession httpSession) {
+    public String view(Model model, HttpSession httpSession, final HttpServletRequest request) {
+        String requestUri = request.getRequestURI(); //请求完整路径，可用于登陆后跳转
+        logger.info("视图请求 requestUri={}", requestUri);
+
         TblUser tblUser = (TblUser) httpSession.getAttribute("user");
         if (tblUser != null) {
             model.addAttribute("uid", tblUser.getUid());
