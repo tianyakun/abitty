03d72ff6fed74464db6981740f60ceb70f9354f4
yakun.tian <yakun.tian@aliyun.com>
2017-07-04 19:15:07 +0800

    eit

--- src/main/java/com/abitty/controller/LoginController.java
+++ src/main/java/com/abitty/controller/LoginController.java
@@ -77,13 +77,14 @@ public class LoginController {
 
                 logger.info("login JSESSIONID={}", session.getId());
 
-//                Cookie cookie = new Cookie("JSESSIONID", session.getId());
+                Cookie cookie = new Cookie("JSESSIONID", session.getId());
+                response.addCookie(cookie);
+
 //                cookie.setSecure(true);
 //                cookie.setHttpOnly(true);
 //                cookie.setMaxAge(7 * 24 * 3600);  // 客户端的JSESSIONID保存7天
 //                cookie.setPath("/");
 
-//                response.addCookie(cookie);
 
                 logger.info("用户登录成功 user={}", session.getAttribute("user"));
 
