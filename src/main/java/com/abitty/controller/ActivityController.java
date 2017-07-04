91b446ca8eca9fc11c0ae81a6cb9d6a06f20d3e1
yakun.tian <yakun.tian@aliyun.com>
2017-07-04 14:45:07 +0800

    edit

--- src/main/java/com/abitty/controller/ActivityController.java
+++ src/main/java/com/abitty/controller/ActivityController.java
@@ -1,34 +1,21 @@
 package com.abitty.controller;
 
-import com.abitty.dto.AddressDto;
 import com.abitty.dto.ResponseDto;
 import com.abitty.entity.TblActivityBanner;
-import com.abitty.entity.TblAddress;
-import com.abitty.entity.TblOrderInfo;
 import com.abitty.enums.ExceptionEnum;
 import com.abitty.service.ActivityBannerService;
-import com.abitty.service.AddressService;
-import com.abitty.utils.ParamChecker;
 import com.abitty.vo.ActivityBannerVo;
-import com.abitty.vo.OrderInfoVo;
 import com.google.common.base.Function;
-import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.common.primitives.Ints;
 import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by yak on 17/6/15.
@@ -59,10 +46,12 @@ public class ActivityController {
 
             responseDto.setRetCode(ExceptionEnum.SUCCESS.getErrorCode());
             responseDto.setRetMsg(ExceptionEnum.SUCCESS.getErrorCode());
+            responseDto.setChineseMsg(ExceptionEnum.SUCCESS.getChineseMessage());
         } catch (Exception e) {
             logger.error("获取活动banner列表异常", e);
             responseDto.setRetCode(ExceptionEnum.SYSTEM_ERROR.getErrorCode());
             responseDto.setRetMsg(ExceptionEnum.SYSTEM_ERROR.getErrorCode());
+            responseDto.setChineseMsg(ExceptionEnum.SYSTEM_ERROR.getChineseMessage());
         }
 
         logger.info("获取活动banner列表返回 responseDto={}", responseDto);
--- src/test/java/com/abitty/BaseTest.java
+++ src/test/java/com/abitty/BaseTest.java
@@ -9,6 +9,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({ "classpath:applicationContext.xml" })
+@ContextConfiguration({ "classpath:/testContext.xml" })
 public class BaseTest {
 }
